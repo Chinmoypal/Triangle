4e32d50be96904c9e1f22ebbcf6b25143f0c7f12
Chinmoypal <155445088+Chinmoypal@users.noreply.github.com>
2024-11-15 13:11:53 +0530

    Add files via upload

---
+++ Triangle.js
@@ -0,0 +1,13 @@
+const printTriangle = function(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+            row += j;
+        }
+        console.log(row);
+    }
+};
+
+// Example usage:
+printTriangle(4);
+
